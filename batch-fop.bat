3a16449f688c033f7294e90801600e08020cf352
Mark A. Matienzo <mark@matienzo.org>
2012-04-02 17:32:50 -0400

    Just send everything to the log file

--- batch-fop.bat
+++ batch-fop.bat
@@ -6,17 +6,25 @@ setlocal EnableDelayedExpansion
 set err=0
 pushd c:\Program Files\facc-xslfo
 
+set logfile=D:\EAD2PDF\logs\%date:~-4%_%date:~4,2%_%date:~7,2%.txt
+@type nul >> %logfile% & copy %logfile% +,, > nul
+echo Writing logs to %logfile%.
+
+echo. >> %logfile%
+echo ------------------------------- >> %logfile%
+echo %date% %time% >> %logfile%
+
 for %%x in (d:\EAD2PDF\in\*.xml) do (
-  echo Transforming %%~dpnx.xml to D:\EAD2PDF\out\%%~nx.pdf ...
-  call fop-saxon -c fop.xconf -xml %%~dpnx.xml -xsl yul.ead2002.pdf.xsl -pdf d:\EAD2PDF\out\%%~nx.pdf
+  echo Transforming %%~dpnx.xml to D:\EAD2PDF\out\%%~nx.pdf ... >> %logfile% 
+  call fop-saxon -c fop.xconf -xml %%~dpnx.xml -xsl yul.ead2002.pdf.xsl -pdf d:\EAD2PDF\out\%%~nx.pdf >> %logfile% 2>&1 
   set err=!errorlevel!
   if !errorlevel! EQU 0 (
-    echo Transformation of %%~dpnx.xml succeeded. Deleting file.
-    echo.
+    echo Transformation of %%~dpnx.xml succeeded. Deleting file. >> %logfile%
+    echo. >> %logfile%
     del %%~dpnx.xml
     ) else (
-    echo Error: Transformation of %%~dpnx.xml failed. Exiting.
-    echo.
+    echo Error: Transformation of %%~dpnx.xml failed. Exiting. >> %logfile%
+    echo. >> %logfile%
     exit /b %err%
     )
   )
