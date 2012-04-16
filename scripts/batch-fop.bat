35c1cc4aef70c329253f6bca0db8ee557c3a6447
Mark A. Matienzo <mark@matienzo.org>
2012-04-16 18:49:16 -0400

    Fix redundant copying of logfile and path info; fix typo in README

--- scripts/batch-fop.bat
+++ scripts/batch-fop.bat
@@ -7,7 +7,7 @@ set err=0
 pushd %~dp0
 
 set logfile=..\logs\%date:~-4%_%date:~4,2%_%date:~7,2%.txt
-@type nul >> %logfile% & copy %logfile% +,, > nul
+@type nul >> %logfile% > nul
 echo Writing logs to %logfile%.
 
 echo. >> %logfile%
@@ -15,15 +15,15 @@ echo ------------------------------- >> %logfile%
 echo %date% %time% >> %logfile%
 
 for %%x in (..\in\*.xml) do (
-  echo Transforming in\%%~dpnx.xml to out\%%~nx.pdf ... >> %logfile% 
+  echo Transforming %%~dpnx.xml to out\%%~nx.pdf ... >> %logfile% 
   call fop-saxon -c fop.xconf -xml %%~dpnx.xml -xsl xslt\yul.ead2002.pdf.xsl -pdf ..\out\%%~nx.pdf >> %logfile% 2>&1 
   set err=!errorlevel!
   if !errorlevel! EQU 0 (
-    echo Transformation of in\%%~dpnx.xml succeeded. Moving file to out\%%~nx.xml >> %logfile%
+    echo Transformation of %%~dpnx.xml succeeded. Moving file to out\%%~nx.xml >> %logfile%
     move %%~dpnx.xml ..\out >> %logfile%
     echo. >> %logfile%
     ) else (
-    echo Error: Transformation of in\%%~dpnx.xml failed. Exiting. >> %logfile%
+    echo Error: Transformation of %%~dpnx.xml failed. Exiting. >> %logfile%
     echo. >> %logfile%
     exit /b %err%
     )
